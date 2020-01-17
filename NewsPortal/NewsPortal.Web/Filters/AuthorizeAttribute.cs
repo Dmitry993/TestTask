3cb0448556e71f32cb2f49b45c722cf5927a0c66
dmitry_yaruta <d.yaruta@itransition.com>
2020-01-17 12:40:45 +0300

    add check null and redirect to login

--- NewsPortal/NewsPortal.Web/Filters/AuthorizeAttribute.cs
+++ NewsPortal/NewsPortal.Web/Filters/AuthorizeAttribute.cs
@@ -7,11 +7,12 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Security.Claims;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace NewsPortal.Web.Attributes
 {
-    public class CustomAuthAttribute : AuthorizeAttribute, IAuthorizationFilter
+    public class CustomAuthorizeAttribute : AuthorizeAttribute, IAuthorizationFilter
     {
         public const string SessionKey = "_AccessToken";
         public void OnAuthorization(AuthorizationFilterContext context)
@@ -19,11 +20,18 @@ namespace NewsPortal.Web.Attributes
             var userToken = context.HttpContext.User.FindFirst("Access_token");
             var serverToken = context.HttpContext.Session.GetString(SessionKey);
 
-            if (userToken.Equals(serverToken))
+            if (userToken == null | serverToken == null)
             {
+                context.Result = new RedirectToActionResult("Login", "Auth", null);
                 return;
+            }          
+            
+            if (!serverToken.Equals(userToken.Value))
+            {
+                context.Result = new BadRequestObjectResult("User is not valid");
+                Thread.Sleep(3000);
+                context.Result = new RedirectToActionResult("GoogleSignIn", "Auth", null);
             }
-
         }
     }
 }
