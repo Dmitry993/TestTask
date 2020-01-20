7fec7490c4a51e602b76cb31b8a2e7adc0d5f6d9
dmitry_yaruta <d.yaruta@itransition.com>
2020-01-20 18:39:18 +0300

    add EntityFramework and db context

---
+++ NewsPortal/NewsPortal.Data/Context/NewsPortalDbContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using NewsPortal.Data.Model;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace NewsPortal.Data.Context
+{
+    public class NewsPortalDbContext : DbContext
+    {
+        public NewsPortalDbContext(DbContextOptions<NewsPortalDbContext> options)
+            : base(options: options)
+        { }
+        public DbSet<ApplicationUserDb> Users { get; set; }
+    }
+}
