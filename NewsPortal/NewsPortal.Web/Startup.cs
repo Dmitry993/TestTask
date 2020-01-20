45daa1bab77ab94409a480c9bdfc3fe0a752d50a
dmitry_yaruta <d.yaruta@itransition.com>
2020-01-20 18:52:30 +0300

    add and integrate Autofac into app

--- NewsPortal/NewsPortal.Web/Startup.cs
+++ NewsPortal/NewsPortal.Web/Startup.cs
@@ -1,67 +1,85 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+using Autofac;
+using Autofac.Extensions.DependencyInjection;
+using AutoMapper;
 using Microsoft.AspNetCore.Authentication.Cookies;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.Http;
-using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using NewsPortal.Data.Context;
+using NewsPortal.Logic;
 using NewsPortal.Logic.Model;
 
 namespace NewsPortal.Web
 {
     public class Startup
-    {     
+    {
         // This method gets called by the runtime. Use this method to add services to the container.
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
-        public Startup(IConfiguration configuration)
+        public Startup(IWebHostEnvironment env)
         {
-            Configuration = configuration;
+            var builder = new ConfigurationBuilder()
+        .SetBasePath(env.ContentRootPath)
+        .AddJsonFile("appsettings.json", optional: true, reloadOnChange: true)
+        .AddJsonFile($"appsettings.{env.EnvironmentName}.json", optional: true)
+        .AddEnvironmentVariables();        
+            this.Configuration = builder.Build();
         }
 
-        public IConfiguration Configuration { get; }
+        public IConfigurationRoot Configuration { get; private set; }
+
+        public ILifetimeScope AutofacContainer { get; private set; }
+
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddOptions();
+            services.AddAutoMapper(typeof(ApplicationUser).Assembly);
             services.AddMvc(options =>
             {
                 options.EnableEndpointRouting = false;
-            });                 
+            });
 
             services.AddAuthentication(options =>
             {
                 options.DefaultSignInScheme = CookieAuthenticationDefaults.AuthenticationScheme;
                 options.DefaultAuthenticateScheme = CookieAuthenticationDefaults.AuthenticationScheme;
-                options.DefaultChallengeScheme = CookieAuthenticationDefaults.AuthenticationScheme;                
-            })        
-            .AddCookie(options=>
+                options.DefaultChallengeScheme = CookieAuthenticationDefaults.AuthenticationScheme;
+            })
+            .AddCookie(options =>
             {
-                options.Cookie.Name = "AuthorizationToken";                
-            });              
+                options.Cookie.Name = "AuthorizationToken";
+            });
+
+            services.AddDbContext<NewsPortalDbContext>(options =>
+             options.UseSqlServer(Configuration["ConnectionStrings:NewsPortalDbContext"]));
+
+            services.AddLogging(logging =>
+            {
+                logging.AddConfiguration(this.Configuration.GetSection("Logging"));
+                logging.AddConsole();
+                logging.AddDebug();
+            });
+        }
+
+        public void ConfigureContainer(ContainerBuilder builder)
+        {
+            // Register your own things directly with Autofac, like:
+            //builder.RegisterModule();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        public void Configure(IApplicationBuilder app)
         {
-            if (env.IsDevelopment())
-            {
-                app.UseDeveloperExceptionPage();
-            }
-            else
-            {
-                app.UseExceptionHandler("/Error");
-                app.UseHsts();
-            }
+            this.AutofacContainer = app.ApplicationServices.GetAutofacRoot();            
 
             app.UseHttpsRedirection();
             app.UseStaticFiles();
-            app.UseRouting();           
+            app.UseRouting();
             app.UseAuthentication();
             app.UseAuthorization();
-            app.UseCookiePolicy();           
+            app.UseCookiePolicy();
             app.UseMvc(routes =>
             {
                 routes.MapRoute(
