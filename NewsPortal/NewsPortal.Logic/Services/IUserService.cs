90dceb39373cc201564117284c6d4deba9646765
dmitry_yaruta <d.yaruta@itransition.com>
2020-01-21 18:42:11 +0300

    add user service

---
+++ NewsPortal/NewsPortal.Logic/Services/IUserService.cs
@@ -0,0 +1,15 @@
+﻿using NewsPortal.Logic.Model;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace NewsPortal.Logic.Services
+{
+    public interface IUserService
+    {
+        IEnumerable<ApplicationUser> GetAllUser();
+        ApplicationUser Get(int id);
+        void CreateUser(ApplicationUser item);
+        bool UserExists(int id);
+    }
+}
---
+++ NewsPortal/NewsPortal.Logic/Services/UserService.cs
@@ -0,0 +1,45 @@
+﻿using AutoMapper;
+using NewsPortal.Data.Model;
+using NewsPortal.Data.Repositories;
+using NewsPortal.Logic.Model;
+using System;
+using System.Collections.Generic;
+
+namespace NewsPortal.Logic.Services
+{
+    public class UserService : IUserService
+    {
+        private readonly IMapper _mapper;
+        private readonly IRepository<ApplicationUserDb> _repository;         
+
+        public UserService(IMapper mapper, IRepository<ApplicationUserDb> repository)
+        {
+            _repository = repository;
+            _mapper = mapper;
+        }
+
+        public void CreateUser(ApplicationUser item)
+        {
+            _repository.Create(_mapper.Map<ApplicationUserDb>(item));
+        }
+
+        public bool UserExists(int id)
+        {
+            if (_repository.Get(id) == null)
+            {
+                return false;
+            }
+            return true;
+        }
+
+        public ApplicationUser Get(int id)
+        {
+            return _mapper.Map<ApplicationUser>(_repository.Get(id));
+        }
+
+        public IEnumerable<ApplicationUser> GetAllUser()
+        {
+            return _mapper.Map<IEnumerable<ApplicationUserDb>, IEnumerable<ApplicationUser>>(_repository.GetAll()); ;
+        }
+    }
+}
