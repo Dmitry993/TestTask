5e3cca320276abdd9d3cbe0fde40d39024866cf9
dmitry_yaruta <d.yaruta@itransition.com>
2020-01-21 18:41:40 +0300

    add AutofacModule and register dependencies

---
+++ NewsPortal/NewsPortal.Web/AutofacModule.cs
@@ -0,0 +1,20 @@
+﻿using Autofac;
+using NewsPortal.Data.Model;
+using NewsPortal.Data.Repositories;
+using NewsPortal.Logic.Services;
+
+namespace NewsPortal.Web
+{
+    public class AutofacModule : Module
+    {        protected override void Load(ContainerBuilder builder)
+        {
+            builder.RegisterType<UserRepository>()
+               .As<IRepository<ApplicationUserDb>>()
+               .InstancePerLifetimeScope();
+
+            builder.RegisterType<UserService>()
+                .As<IUserService>()
+                .InstancePerLifetimeScope();
+        }
+    }
+}
