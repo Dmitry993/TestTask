0681375505ec2e216cb0a58a7a6af2c297da24db
dmitry_yaruta <d.yaruta@itransition.com>
2020-01-23 19:03:17 +0300

    add UserProfile

---
+++ NewsPortal/NewsPortal.Web/Profiles/UserProfile.cs
@@ -0,0 +1,25 @@
+﻿using AutoMapper;
+using NewsPortal.Logic.Model;
+using OAuth2.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace NewsPortal.Web.Profiles
+{
+    public class UserProfile : Profile
+    {
+        public UserProfile()
+        {
+            CreateMap<UserInfo, ApplicationUser>()
+                .ForMember(b => b.Id, opt => opt.Ignore())
+                .ForMember(b => b.GoogleId, opt => opt.MapFrom(b => b.Id))
+                .ForMember(b => b.UserName, opt => opt.MapFrom(b => b.Email.Split('@', StringSplitOptions.None)[0]))
+                .ForMember(b => b.Email, opt => opt.MapFrom(b => b.Email))
+                .ForMember(b => b.FirstName, opt => opt.MapFrom(b => b.FirstName))
+                .ForMember(b => b.LastName, opt => opt.MapFrom(b => b.LastName))
+                .ReverseMap();
+        }
+    }
+}
