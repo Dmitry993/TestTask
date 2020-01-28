ff604baaf2c9e7260fd1619ef6be70b18678550f
dmitry_yaruta <d.yaruta@itransition.com>
2020-01-28 18:12:44 +0300

    add Post service

---
+++ NewsPortal/NewsPortal.Logic/Services/IPostService.cs
@@ -0,0 +1,15 @@
+﻿using NewsPortal.Logic.Model;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace NewsPortal.Logic.Services
+{
+    public interface IPostService
+    {
+        Task<IEnumerable<UserPost>> GetAllPostsAsync();
+
+        Task<UserPost> GetPostAsync(int id);       
+
+        Task<UserPost> CreatePostAsync(UserPost userPost);       
+    }
+}
