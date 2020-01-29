207749a8a9dfcfe7b3dfc7a36aca3e5a97085bd1
dmitry_yaruta <d.yaruta@itransition.com>
2020-01-29 12:42:08 +0300

    add Post controller

---
+++ NewsPortal/NewsPortal.Web/Controllers/PostController.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using NewsPortal.Logic.Model;
+using NewsPortal.Logic.Services;
+using NewsPortal.Web.Attributes;
+
+namespace NewsPortal.Web.Controllers
+{
+    [CustomAuthorize]
+    public class PostController : Controller
+    {
+        private IPostService _postService;
+
+        public PostController(IPostService postService)
+        {
+            _postService = postService;
+        }
+
+        public IActionResult Post(UserPost userPost)
+        {
+            return View("Post", userPost);
+        }
+        
+        public IActionResult CreatePost()
+        {
+            return View("Create");
+        }        
+
+        public async Task<IActionResult> Create(UserPost userPost)
+        {
+            var stringId = HttpContext.Request.Cookies["UserId"];
+            var id = Int32.Parse(stringId);
+
+            userPost.AuthorId = id;
+            await _postService.CreatePostAsync(userPost);
+
+            if (userPost == null)
+            {
+                return BadRequest();
+            }
+            return RedirectToAction("Index", "Home");
+        }        
+    }
+}
