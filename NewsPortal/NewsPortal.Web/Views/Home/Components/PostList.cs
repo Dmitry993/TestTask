3001e45653eb4a7791a8b5919e16be097dedc26d
dmitry_yaruta <d.yaruta@itransition.com>
2020-01-29 12:43:28 +0300

    add PostList component

---
+++ NewsPortal/NewsPortal.Web/Views/Home/Components/PostList.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using NewsPortal.Logic.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace NewsPortal.Web.Views.Components
+{
+    public class PostListViewComponent : ViewComponent
+    {
+        private IPostService _postService;
+
+        public PostListViewComponent(IPostService postService)
+        {
+            _postService = postService;
+        }
+
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            var posts = await _postService.GetAllPostsAsync();
+
+            return View("AllPosts", posts); 
+        }
+    }
+}
