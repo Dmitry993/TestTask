922954fde4fa74bfb9a9167b4763ccec5c695b4e
dmitry_yaruta <d.yaruta@itransition.com>
2020-02-04 19:12:50 +0300

    add Comment service

---
+++ NewsPortal/NewsPortal.Logic/Services/CommentService.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using AutoMapper;
+using NewsPortal.Data.Models;
+using NewsPortal.Data.Repositories;
+using NewsPortal.Logic.Model;
+
+namespace NewsPortal.Logic.Services
+{
+    public class CommentService : ICommentService
+    {
+        private readonly IMapper _mapper;
+        private readonly ICommentRepository _repository;
+
+        public CommentService(ICommentRepository repository, IMapper mapper)
+        {
+            _repository = repository;
+            _mapper = mapper;
+        }
+
+        public async Task<IEnumerable<UserComment>> GetAllCommentsAsync()
+        {
+            var comments = await _repository.GetAllAsync();
+            return _mapper.Map<IEnumerable<Comment>, IEnumerable<UserComment>>(comments);
+        }
+
+        public async Task<UserComment> GetCommentAsync(int id)
+        {
+            var comment = await _repository.GetAsync(id);
+            return _mapper.Map<UserComment>(comment);
+        }
+
+        public async Task<IEnumerable<UserComment>> GetPostCommentsAsync(int id)
+        {
+            var postComments = new List<UserComment>();
+            var comments = await _repository.GetAllAsync();
+            var userComments = _mapper.Map<List<UserComment>>(comments);
+
+            foreach (var item in userComments)
+            {
+                if (item.PostId == id)
+                {
+                    postComments.Add(item);
+                }
+
+                if(postComments.Any(comment => comment.Id == item.ParentId))
+                {
+                    postComments.Where(comment => comment.Id == item.ParentId)
+                        .ToList()
+                        .ForEach(comment => comment.Reply.Add(item));
+                }
+            }
+
+            return postComments;
+        }
+
+        public async Task<UserComment> AddReplyToCommentAsync(UserComment userComment)
+        {
+            var comment = _mapper.Map<Comment>(userComment);
+            comment.Created = DateTime.UtcNow;
+            await _repository.CreateAsync(comment);
+            await _repository.SaveAsync();
+            return _mapper.Map<UserComment>(comment);
+        } 
+
+        public async Task<UserComment> CreateCommentAsync(UserComment userComment)
+        {
+            var comment = _mapper.Map<Comment>(userComment);
+            comment.Created = DateTime.UtcNow;
+            await _repository.CreateAsync(comment);
+            await _repository.SaveAsync();
+            return _mapper.Map<UserComment>(comment);
+        }
+    }
+}
---
+++ NewsPortal/NewsPortal.Logic/Services/ICommentService.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using NewsPortal.Logic.Model;
+
+namespace NewsPortal.Logic.Services
+{
+    public interface ICommentService
+    {
+        Task<IEnumerable<UserComment>> GetAllCommentsAsync();
+
+        Task<UserComment> GetCommentAsync(int id);
+
+        Task<UserComment> CreateCommentAsync(UserComment userPost);
+
+        Task<UserComment> AddReplyToCommentAsync(UserComment userComment);
+
+        Task<IEnumerable<UserComment>> GetPostCommentsAsync(int id);
+    }
+}
