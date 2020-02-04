101cff8cd0469d7be58319cf60ffba6160974fa8
dmitry_yaruta <d.yaruta@itransition.com>
2020-02-04 19:13:54 +0300

    add AddCommentComponent

---
+++ NewsPortal/NewsPortal.Web/Components/AddCommentComponent.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using Microsoft.AspNetCore.Mvc;
+using NewsPortal.Logic.Services;
+using System.Threading.Tasks;
+using NewsPortal.Logic.Model;
+
+namespace NewsPortal.Web.Components
+{
+    [ViewComponent(Name = "AddComment")]
+    public class AddCommentComponent : ViewComponent
+    {
+        private readonly IUserService _userService;
+
+        public AddCommentComponent(IUserService userService)
+        {
+            _userService = userService;
+        }
+
+        public async Task<IViewComponentResult> InvokeAsync(int postId, string userName)
+        {
+            var stringId = HttpContext.Request.Cookies["UserId"];
+            var id = Int32.Parse(stringId);
+
+            var user = await _userService.GetUserAsync(id);
+
+            var comment = new UserComment()
+            {
+                PostId = postId,
+                UserName = user.UserName
+            };
+
+            return View("/Views/Comment/Create.cshtml", comment);
+        }
+    }
+}
