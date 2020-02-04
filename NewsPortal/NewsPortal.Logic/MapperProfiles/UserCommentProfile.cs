2b8a2d824d1f719761fef2548f189bb922997bf4
dmitry_yaruta <d.yaruta@itransition.com>
2020-02-04 19:15:27 +0300

    add UserComponentProfile

---
+++ NewsPortal/NewsPortal.Logic/MapperProfiles/UserCommentProfile.cs
@@ -0,0 +1,17 @@
+﻿using AutoMapper;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using NewsPortal.Logic.Model;
+using NewsPortal.Data.Models;
+
+namespace NewsPortal.Logic.MapperProfiles
+{
+    public class UserCommentProfile  : Profile
+    {
+        public UserCommentProfile()
+        {
+            CreateMap<Comment, UserComment>().ReverseMap();
+        }
+    }
+}
