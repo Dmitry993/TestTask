31348352cf623e87dc2fa812516f5769d74f2c8d
dmitry_yaruta <d.yaruta@itransition.com>
2020-02-04 19:14:34 +0300

    add PostListComponent

---
+++ NewsPortal/NewsPortal.Web/Components/CommentListComponent.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using NewsPortal.Logic.Services;
+
+namespace NewsPortal.Web.Components
+{
+    [ViewComponent(Name = "CommentList")]
+    public class CommentListComponent : ViewComponent
+    {
+        private ICommentService _commentService;
+
+        public CommentListComponent(ICommentService commentService)
+        {
+            _commentService = commentService;
+        }
+
+        public async Task<IViewComponentResult> InvokeAsync(int postId)
+        {
+            if (postId == 0)
+            {
+                return View("/Views/Home/Index.cshtml");
+            }
+
+            var comments = await _commentService.GetPostCommentsAsync(postId);
+
+            return View("/Views/Comment/Comment.cshtml", comments);
+        }
+    }
+}
