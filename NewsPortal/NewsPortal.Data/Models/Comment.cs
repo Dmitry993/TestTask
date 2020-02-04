d4a8f7ce741adf643eea4a0d049148581b86aa36
dmitry_yaruta <d.yaruta@itransition.com>
2020-02-04 19:09:56 +0300

    add model Comment

---
+++ NewsPortal/NewsPortal.Data/Models/Comment.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace NewsPortal.Data.Models
+{
+    public class Comment
+    {
+        public int Id { get; set; }
+
+        public int? ParentId { get; set; }
+
+        public int? PostId { get; set; }
+
+        public string UserName { get; set; }
+
+        public string Description { get; set; }
+    
+        public DateTime Created { get; set; }
+    }
+}
