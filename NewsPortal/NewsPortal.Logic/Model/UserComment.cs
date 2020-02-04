2f71a1896a6c92ce9215a7b465c67c77cd21091f
dmitry_yaruta <d.yaruta@itransition.com>
2020-02-04 19:15:46 +0300

    add UserComment

---
+++ NewsPortal/NewsPortal.Logic/Model/UserComment.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace NewsPortal.Logic.Model
+{
+    public class UserComment
+    {
+        public int Id { get; set; }
+
+        public int? ParentId { get; set; }
+
+        public int? PostId { get; set; }
+
+        public string UserName { get; set; }
+
+        public string Description { get; set; }
+
+        public List<UserComment> Reply { get; set; } = new List<UserComment>();
+    }
+}
