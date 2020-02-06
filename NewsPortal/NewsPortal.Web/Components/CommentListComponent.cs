6c25eb849fffc53b17085049556f83a5322576f8
dmitry_yaruta <d.yaruta@itransition.com>
2020-02-06 12:53:30 +0300

    change CommentList component

--- NewsPortal/NewsPortal.Web/Components/CommentListComponent.cs
+++ NewsPortal/NewsPortal.Web/Components/CommentListComponent.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using NewsPortal.Logic.Model;
 using NewsPortal.Logic.Services;
 
 namespace NewsPortal.Web.Components
@@ -17,11 +18,16 @@ namespace NewsPortal.Web.Components
             _commentService = commentService;
         }
 
-        public async Task<IViewComponentResult> InvokeAsync(int postId)
+        public async Task<IViewComponentResult> InvokeAsync(int postId, List<UserComment> reply, int commentId)
         {
-            if (postId == 0)
+            if (postId == 0 & commentId == 0)
             {
                 return View("/Views/Home/Index.cshtml");
+            }
+
+            if (reply != null)
+            {
+                return View("/Views/Comment/ReplyToComment.cshtml", reply);
             }
 
             var comments = await _commentService.GetPostCommentsAsync(postId);
