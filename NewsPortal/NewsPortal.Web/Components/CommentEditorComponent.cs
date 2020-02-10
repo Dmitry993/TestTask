7e0efb5fd6934a8abbe36f20830e6a7bb0986434
dmitry_yaruta <d.yaruta@itransition.com>
2020-02-10 12:09:09 +0300

    change CommentEditor component

--- NewsPortal/NewsPortal.Web/Components/CommentEditorComponent.cs
+++ NewsPortal/NewsPortal.Web/Components/CommentEditorComponent.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 using Microsoft.AspNetCore.Mvc;
 using NewsPortal.Logic.Services;
 using System.Threading.Tasks;
@@ -16,19 +18,32 @@ namespace NewsPortal.Web.Components
             _userService = userService;
         }
 
-        public async Task<IViewComponentResult> InvokeAsync(int postId, int? commentId = null)
+        public async Task<IViewComponentResult> InvokeAsync(List<Comment> comments, int postId, int? commentId = null)
         {
             var userIdString = HttpContext.Request.Cookies["UserId"];
             var userId = Int32.Parse(userIdString);
 
-            var comment = new Comment()
+            if (comments != null && comments.Any())
+            {
+                var cooment = comments.FirstOrDefault();
+
+                var replyToComment = new Comment()
+                {
+                    PostId = cooment.PostId,
+                    ParentId = cooment.Id,
+                    UserId = userId
+                };
+                return View("/Views/Comment/CommentEditor.cshtml", cooment);
+            }
+
+            var postComment = new Comment()
             {
                 PostId = postId,
                 ParentId = commentId,
                 UserId = userId
             };
 
-            return View("/Views/Comment/CommentEditor.cshtml", comment);
+            return View("/Views/Comment/CommentEditor.cshtml", postComment);
         }
     }
 }
