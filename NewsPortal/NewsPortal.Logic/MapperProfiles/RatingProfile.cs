99d0177c1686929e92f57084970e0d047bf1f516
dmitry_yaruta <d.yaruta@itransition.com>
2020-02-11 13:38:49 +0300

    add Rating profile

---
+++ NewsPortal/NewsPortal.Logic/MapperProfiles/RatingProfile.cs
@@ -0,0 +1,12 @@
+﻿using AutoMapper;
+
+namespace NewsPortal.Logic.MapperProfiles
+{
+    public class RatingProfile : Profile
+    {
+        public RatingProfile()
+        {
+            CreateMap<Data.Models.Rating, Logic.Models.Rating>().ReverseMap();
+        }
+    }
+}
