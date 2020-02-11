a6b6e9d03fb19f6dae2b775d4436e339227f9893
dmitry_yaruta <d.yaruta@itransition.com>
2020-02-11 13:44:48 +0300

    add Rating contoller

---
+++ NewsPortal/NewsPortal.Web/Controllers/RatingController.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using NewsPortal.Logic.Models;
+using NewsPortal.Logic.Services;
+
+namespace NewsPortal.Web.Controllers
+{
+    public class RatingController : Controller
+    {
+        private IRatingService _service;
+
+        public RatingController(IRatingService service)
+        {
+            _service = service;
+        }
+
+        public async Task<IActionResult> UpRating(int postId)
+        {
+            await _service.UpPostRatingAsync(postId);
+
+            return RedirectToAction("GetPostById", "Post", new { id = postId });
+        }
+
+        public async Task<IActionResult> DownRating(int postId)
+        {
+            await _service.DownPostRatingAsync(postId);
+
+            return RedirectToAction("GetPostById", "Post", new { id = postId });
+        }
+    }
+}
