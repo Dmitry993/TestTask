a99ebf306ce48b81f5165a8e81354b3d6abebb94
dmitry_yaruta <d.yaruta@itransition.com>
2020-02-12 12:58:18 +0300

    refactor

--- NewsPortal/NewsPortal.Data/Context/NewsPortalDbContext.cs
+++ NewsPortal/NewsPortal.Data/Context/NewsPortalDbContext.cs
@@ -15,7 +15,7 @@ namespace NewsPortal.Data.Context
         public DbSet<User> Users { get; set; }
         public DbSet<Post> Posts { get; set; }
         public DbSet<Comment> Comments { get; set; }
-        public DbSet<Rating> Ratings { get; set; }
+        public DbSet<PostRating> Ratings { get; set; }
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
@@ -35,11 +35,6 @@ namespace NewsPortal.Data.Context
                 .HasMany<Comment>()
                 .WithOne()
                 .HasForeignKey(comment => comment.PostId);
-
-            modelBuilder.Entity<Post>()
-                .HasOne(c => c.Rating)
-                .WithMany()
-                .HasForeignKey(post => post.RatingId);
 
             modelBuilder.Entity<Comment>()
                 .HasOne<Comment>()
