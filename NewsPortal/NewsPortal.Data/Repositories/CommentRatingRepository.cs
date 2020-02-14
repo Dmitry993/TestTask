86760a668cc67eb78bc0c1417908f7679361dd15
dmitry_yaruta <d.yaruta@itransition.com>
2020-02-14 12:06:34 +0300

    add CommentRating model and CommentRating repository

---
+++ NewsPortal/NewsPortal.Data/Repositories/CommentRatingRepository.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using NewsPortal.Data.Context;
+using NewsPortal.Data.Models;
+
+namespace NewsPortal.Data.Repositories
+{
+    public class CommentRatingRepository : Repository<CommentRating>, ICommentRatingRepository
+    {
+        private readonly NewsPortalDbContext _context;
+
+        public CommentRatingRepository(NewsPortalDbContext context) : base(context)
+        {
+            _context = context;
+        }
+
+        public async Task<CommentRating> FindItem(int commentId, int userId)
+        {
+            return await _context.CommentRatings
+                .Where(rating => rating.CommentId == commentId && rating.UserId == userId)
+                .FirstOrDefaultAsync();
+        }
+
+        public async Task DeleteItem(int commentId, int userId)
+        {
+            var rating = await _context.CommentRatings
+                .Where(rating => rating.CommentId == commentId && rating.UserId == userId)
+                .FirstOrDefaultAsync();
+            if (rating != null)
+            {
+                _context.CommentRatings.Remove(rating);
+            }
+        }
+    }
+}
---
+++ NewsPortal/NewsPortal.Data/Repositories/ICommentRatingRepository.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using NewsPortal.Data.Models;
+
+namespace NewsPortal.Data.Repositories
+{
+    public interface ICommentRatingRepository: IRepository<CommentRating>
+    {
+        Task<CommentRating> FindItem(int commentId, int userId);
+
+        Task DeleteItem(int commentId, int userId);
+    }
+}
